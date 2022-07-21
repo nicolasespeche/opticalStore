6a073d49b1a6e0b0a200da911f983b9b333de9fb
Nicolas Espeche <nicolasespeche94@gmail.com>
2022-07-20 22:09:02 -0300

    Objeto con Array

--- index.js
+++ index.js
@@ -16,27 +16,51 @@
 
 /*--------Desafio de funciones js--------*/
 
-function pago (modoDePago){
-    if(modoDePago === "efectivo" || modoDePago === "Efectivo"){
-        console.log("El precio de contado tiene un 10% de descuento")
-    }
-    else if(modoDePago === "cuotas" || modoDePago === "Cuotas"){
-        for (let i = 3 ; i <= 12 ; i+=3){
-            if(i === 3){
-                console.log(`Puede hacerlo en ${i} cuotas`)
-            }
-            else if (i === 6 || i === 9){
-                console.log(`en ${i} cuotas`)
-            }
-            else if (i === 12){
-                console.log(`o hasta en ${i} cuotas`)
-            }
-        }
-    }
+// function pago (modoDePago){
+//     if(modoDePago === "efectivo" || modoDePago === "Efectivo"){
+//         console.log("El precio de contado tiene un 10% de descuento")
+//     }
+//     else if(modoDePago === "cuotas" || modoDePago === "Cuotas"){
+//         for (let i = 3 ; i <= 12 ; i+=3){
+//             if(i === 3){
+//                 console.log(`Puede hacerlo en ${i} cuotas`)
+//             }
+//             else if (i === 6 || i === 9){
+//                 console.log(`en ${i} cuotas`)
+//             }
+//             else if (i === 12){
+//                 console.log(`o hasta en ${i} cuotas`)
+//             }
+//         }
+//     }
+// }
+
+// modoDePago= prompt("¿De que manera desea realizar el pago? Efectivo o en Cuotas")
+
+// pago(modoDePago)
+
+nombrePrompt = prompt("Ingrese su Nombre");
+apellidoPrompt = prompt("Ahora ingrese su Apellido");
+edadPrompt = prompt ("¿Cuál es su edad?");
+
+function nuevoCliente (nombre,apellido, edad) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
 }
 
-modoDePago= prompt("¿De que manera desea realizar el pago? Efectivo o en Cuotas")
+cliente1 = new nuevoCliente(nombrePrompt,apellidoPrompt,edadPrompt)
 
-pago(modoDePago)
+arrayNombre = [];
+arrayApellido = [];
+arrayEdad = [];
+
+arrayNombre.push(nombrePrompt);
+arrayApellido.push(apellidoPrompt);
+arrayEdad.push(edadPrompt);
+
+console.log(cliente1)
+
+
 
 
